d66c15d876fa1e40f42b325e01c60184e6bf1c4a
Rohit Anchal <105239963+rohitanchal@users.noreply.github.com>
2024-03-12 23:49:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+  if (typeof s1 !== 'string' || typeof s2 !== 'string') {
+    return -1; // return -1 if any of the inputs is not a string
+  }
+
+  const strLower = s1.toLowerCase();
+  const subStrLower = s2.toLowerCase();
+
+  for (let i = 0; i <= s1.length - s2.length; i++) {
+    if (strLower.substr(i, s2.length) === subStrLower) {
+      return i;
+    }
+  }
+
+  return -1; // return -1 if subStr is not found in str
 }
 
 // Please do not change the code below
